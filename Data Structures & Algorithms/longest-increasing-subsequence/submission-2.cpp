d02b353bddb4925441e34c48b3a6ac3e4c261772
Om Prakash <44462272+Om-Prakash08@users.noreply.github.com>
2026-07-04 19:51:16 +0530

    Add: longest-increasing-subsequence - submission-2

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int>tail;
+        for(int a: nums){
+            //first index where nums[idx]>=a
+            auto it = lower_bound(tail.begin(),tail.end(),a);
+            if(it==tail.end()){
+                tail.push_back(a);
+            }
+            else{
+                *it = a;
+            }
+        }
+        return tail.size();
+    }
+};
